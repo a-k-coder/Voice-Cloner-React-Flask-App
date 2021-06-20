eaee3a4ada23f9eba08df7c92c36029643e2597e
a-k-coder <58370094+a-k-coder@users.noreply.github.com>
2021-06-20 16:06:19 -0700

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -84,8 +84,8 @@ class App extends Component {
     const formData = this.state.formData;
     const result = this.state.result;
     
-//     let path = 'C:\Users\Aruna\Desktop\Springboard\Curriculum\21\21.5\ML-React-App-Template\ML-React-App-Template\ui\resources\output\cartoon-birds-2_daniel-simion.mp3'
-//     let audio = new Audio(path);
+    let path = 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.mp3';
+    let audio = new Audio(path);
 
     return (
       <Container>
@@ -157,7 +157,7 @@ class App extends Component {
               <Form.Group as={Col}>
                 <Button id="audio-button" 
                   block
-                  onClick={() => null}>
+                  onClick={() => audio.play}>
                   Play sound
                 </Button>
               </Form.Group>
