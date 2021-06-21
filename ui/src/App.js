e9e7cecb56b841cb7889011d155e313d2f50ee02
a-k-coder <58370094+a-k-coder@users.noreply.github.com>
2021-06-21 16:04:57 -0700

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
-// import soundfile from 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.wav';
+import soundfile from 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\test1.mp3';
 // import Sound from 'react-sound';
 
 class App extends Component {
@@ -85,8 +85,8 @@ class App extends Component {
     const formData = this.state.formData;
     const result = this.state.result;
     
-    var path = 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.wav';
-    var audio = new Audio(path);
+    
+    var audio = new Audio(soundfile);
     
 
     return (
