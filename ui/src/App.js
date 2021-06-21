41c20b3f3a2c2ddf8906e8915aea8c4ca3cd89c0
a-k-coder <58370094+a-k-coder@users.noreply.github.com>
2021-06-21 15:10:00 -0700

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
-import soundfile from 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.mp3';
+// import soundfile from 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.mp3';
 // import Sound from 'react-sound';
 
 class App extends Component {
@@ -86,7 +86,8 @@ class App extends Component {
     const result = this.state.result;
     
 //     var path = 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.mp3';
-    var audio = new Audio(soundfile);
+    var audio = new Audio();
+    audio.src='C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\output\\cartoon-birds-2_daniel-simion.mp3';
 
     return (
       <Container>
