77235d53f40aaf50cdafc7af64ce075afc7b7872
a-k-coder <58370094+a-k-coder@users.noreply.github.com>
2021-06-24 12:40:09 -0700

    Update app.py

--- service/app.py
+++ service/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, flash, redirect, url_for
 from flask_restplus import Api, Resource, fields
 from sklearn.externals import joblib
+import os
+from werkzeug.utils import secure_filename
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app, 
@@ -14,6 +16,9 @@ model = app.model('Print params',
 				  {'textField1': fields.String(required = True, 
 				  							   description="Text Field 1", 
     					  				 	   help="Text Field 1 cannot be blank")})
+
+UPLOAD_FOLDER = 'C:\\Users\\Aruna\\Desktop\\Springboard\\Curriculum\\21\\21.5\\ML-React-App-Template\\ML-React-App-Template\\ui\\src\\resources\\input'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','wav','mp3','m4a'}
 
 # classifier = joblib.load('classifier.joblib')
 
@@ -49,6 +54,9 @@ class MainClass(Resource):
 
 @name_space.route("/upload")
 class MainClass(Resource):
+	
+	def allowed_file(filename):
+		return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 	def options(self):
 		response = make_response()
@@ -57,25 +65,44 @@ class MainClass(Resource):
 		response.headers.add('Access-Control-Allow-Methods', "*")
 		return response
 
-	@app.expect(model)		
+# 	@app.expect(model)		
 	def post(self):
 		try: 
-			formData = request.json
-			data = [val for val in formData.values()]
-			# prediction = classifier.predict(data)
-			response = jsonify({
-				"statusCode": 200,
-				"status": "Print complete",
-				"result": "Uploaded " + str(data)
-				})
-			response.headers.add('Access-Control-Allow-Origin', '*')
-			return response
+			file = request.files['file']
+			print(file)
+			return "done"
+			
 		except Exception as error:
 			return jsonify({
 				"statusCode": 500,
 				"status": "Could not make prediction",
 				"error": str(error)
 			})
+		def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 		
 @name_space.route("/saveaudio")
 class MainClass(Resource):
