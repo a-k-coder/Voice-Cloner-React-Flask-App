7420a43ee598821fa15b786c8f2293bedf32193d
a-k-coder <58370094+a-k-coder@users.noreply.github.com>
2021-06-24 21:17:25 -0700

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -68,9 +68,6 @@ class App extends Component {
     fetch('http://127.0.0.1:5000/voicecloner/upload', 
       {
         headers: {
-//           'Accept': 'application/json',
-//           'Content-Type': 'application/json',
-          'Content-Type': '*/*',
           'Access-Control-Allow-Origin': '*'
         },
         method: 'POST',
